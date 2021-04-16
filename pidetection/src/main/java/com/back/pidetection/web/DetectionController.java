ecbc2b768567707cca7050ee4f93ce857d6187b0
김태준 <kimtaejun@gimtaejun-ui-MacBookAir.local>
2021-04-16 22:18:40 +0900

    get result Api

---
+++ pidetection/src/main/java/com/back/pidetection/web/DetectionController.java
@@ -0,0 +1,19 @@
+package com.back.pidetection.web;
+
+import com.back.pidetection.web.dto.DetectionResultResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DetectionController {
+
+
+    @GetMapping("/api/face/result")
+    public String result(DetectionResultResponseDto responseDto, Model model){
+        model.addAttribute("result", responseDto);
+        return "detection-result";
+    }
+}
