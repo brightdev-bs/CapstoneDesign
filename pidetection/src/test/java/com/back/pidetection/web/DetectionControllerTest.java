02aacb87232d0d9d05850cf381d151999f1f93ef
김태준 <kimtaejun@gimtaejun-ui-MacBookAir.local>
2021-04-17 13:51:54 +0900

    Web Client frame implementation

---
+++ pidetection/src/test/java/com/back/pidetection/web/DetectionControllerTest.java
@@ -0,0 +1,24 @@
+package com.back.pidetection.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(DetectionController.class)
+public class DetectionControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void connect() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk());
+    }
+}
