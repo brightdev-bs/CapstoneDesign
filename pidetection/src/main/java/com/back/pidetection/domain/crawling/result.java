8b566291cbe8a5cf21cd5d646e881274cc036cee
kimtaejun <kim_taejun@naver.com>
2021-04-18 12:57:58 +0900

    get result (author 수정)

---
+++ pidetection/src/main/java/com/back/pidetection/domain/crawling/result.java
@@ -0,0 +1,15 @@
+package com.back.pidetection.domain.crawling;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class result {
+
+}
