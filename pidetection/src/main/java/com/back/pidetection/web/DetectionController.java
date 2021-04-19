8852c43491a6f4eb0b91344f199bacbf980cdc13
kimtaejun <kim_taejun@naver.com>
2021-04-19 16:31:03 +0900

    get result (author 수정)

--- pidetection/src/main/java/com/back/pidetection/web/DetectionController.java
+++ pidetection/src/main/java/com/back/pidetection/web/DetectionController.java
@@ -1,27 +1,39 @@
 package com.back.pidetection.web;
 
-import com.back.pidetection.web.dto.DetectionResultResponseDto;
-import lombok.RequiredArgsConstructor;
+import com.back.pidetection.web.dto.DetectionResultDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 
+
 @Controller
 public class DetectionController {
+    ArrayList<DetectionResultDto> resultDtos = new ArrayList<>();
 
     @GetMapping("/")
     public String index(){
         return "index";
     }
 
-    @GetMapping("/api/face/result")
-    public String result(DetectionResultResponseDto responseDto, Model model){
-        model.addAttribute("result", responseDto);
+    @GetMapping("/wait-page")
+    public String waitPage(){
+        return "wait-page";
+    }
 
-        return "detection-result";
+    @PostMapping("/api/face/result")
+    public String saveResult(@RequestBody DetectionResultDto resultDto, Model model) {
+        if (resultDto.getImage().length != 0) {
+            resultDtos.add(resultDto);
+            return "wait-page";
+        } else {
+            model.addAttribute("result", resultDtos);
+            return "redirect:detection-result";
+        }
+
     }
 
 }
