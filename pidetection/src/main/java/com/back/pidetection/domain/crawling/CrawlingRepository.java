6cda6c57c6be1fe09dccff2199fe431aa3e07ef1
kimtaejun <kim_taejun@naver.com>
2021-05-10 15:51:52 +0900

    서버에 올리기 위해 repository분리

--- pidetection/src/main/java/com/back/pidetection/domain/crawling/CrawlingRepository.java
+++
@@ -1,6 +0,0 @@
-package com.back.pidetection.domain.crawling;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CrawlingRepository extends JpaRepository<Crawling, Long> {
-}
