5cec6ef5ba17189cc5fd0415c1011f138dcc4e13
kimtaejun <kim_taejun@naver.com>
2021-05-17 15:10:55 +0900

    AIserver cmd 실행 코드

--- AIServer/src/main/java/com/server/ai/aiserver/service/AiService.java
+++ AIServer/src/main/java/com/server/ai/aiserver/service/AiService.java
@@ -1,11 +1,11 @@
 package com.server.ai.aiserver.service;
 
-import com.server.ai.aiserver.web.dto.DetectionRequestDto;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class AiService {
     public void localSave(MultipartFile image) throws IOException {
@@ -28,8 +28,32 @@ public class AiService {
 
     public void run(){
         System.out.println("얼굴 매칭....");
+
         //cmd
-        System.out.println("결과 전송....");
+        try{
+            String cmd[] = new String[4];
+            cmd[0] ="cmd.exe";
+            // 명령어 모두 실해 후 종료 옵션.
+            cmd[1] = "/C";
+            cmd[2] = "pair.txt, .bin 파일 생성";
+            cmd[3] = "얼굴 인식, 결과 전송";
+
+            Runtime runtime = Runtime.getRuntime();
+            Process process = runtime.exec(cmd);
+
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(
+                            process.getInputStream()));
+            
+            String line;
+            while((line =br.readLine()) !=null)
+                System.out.println(line);
+
+        }catch(Throwable t){
+            t.printStackTrace();
+        }
+
+        System.out.println("결과 전송 완료.");
         System.out.println("종료.");
 
 
